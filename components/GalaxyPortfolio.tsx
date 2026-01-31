08ac4759919263720366adabf3595538030cfdc7
bappadeydevelopment <bappa.development@gmail.com>
2026-01-31 20:41:00 +0530

    Initial galaxy portfolio

---
+++ components/GalaxyPortfolio.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const FEATURES = {
+  audio: true,
+  breathing: true,
+  scrollGravity: true,
+  lowPowerFallback: true,
+};
+
+export function GalaxyPortfolio() {
+  return (
+    <div>
+      <h1>Galaxy Portfolio</h1>
+    </div>
+  );
+}
